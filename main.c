9a85d362489f2dd10b0f6102bf9c14f33d5a3af0
YAMAKAWA Yuichi <yuichi.ymkw4a@gmail.com>
2020-07-18 09:00:56 +0900

    init

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct thdata {
+    pthread_t th;
+};
+
+void *thread_func1(void *thdata)
+{
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        printf("thread1 start\n");
+    }
+    return NULL;
+}
+
+
+void *thread_func2(void *thdata)
+{
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        printf("thread2 start\n");
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    int ret = 0;
+    int count;
+    struct thdata *thdata;
+
+    thdata = calloc(sizeof(struct thdata), 2);
+    if(thdata == NULL)
+    {
+        perror("calloc()");
+        return -1;
+    }
+
+    scanf("%d", &count);    
+
+    ret = pthread_create(&thdata[0].th, NULL, thread_func1, &thdata[0]);
+    ret += pthread_create(&thdata[1].th, NULL, thread_func2, &thdata[1]);
+    if(ret != 0)
+    {
+        perror("init pthread_create");
+        return -2;
+    }
+
+    while(1)
+    {
+        scanf("%d", &count);
+        if(count == 0)
+        {
+            break;
+        }
+    }
+
+    free(thdata);
+    return 0;
+}
+
